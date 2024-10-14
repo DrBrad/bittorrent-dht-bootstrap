47e88f410b613532364aca41c7ccb176e5a3f47b
DrBrad <brad@bradeagle.com>
2024-10-14 12:38:07 -0600

    Fix to rust libraries

---
+++ rust/target/debug/deps/bencode-d8bf5768ef905017.d
@@ -0,0 +1,17 @@
+/home/brad/Projects/bittorrent-dht-bootstrap/rust/target/debug/deps/libbencode-d8bf5768ef905017.rmeta: libs/BencodeRust/src/lib.rs libs/BencodeRust/src/variables/mod.rs libs/BencodeRust/src/variables/inter/mod.rs libs/BencodeRust/src/variables/inter/bencode_type.rs libs/BencodeRust/src/variables/inter/bencode_variable.rs libs/BencodeRust/src/variables/bencode_number.rs libs/BencodeRust/src/variables/bencode_object.rs libs/BencodeRust/src/variables/bencode_array.rs libs/BencodeRust/src/variables/bencode_bytes.rs libs/BencodeRust/src/utils/mod.rs libs/BencodeRust/src/utils/ordered_map.rs
+
+/home/brad/Projects/bittorrent-dht-bootstrap/rust/target/debug/deps/libbencode-d8bf5768ef905017.rlib: libs/BencodeRust/src/lib.rs libs/BencodeRust/src/variables/mod.rs libs/BencodeRust/src/variables/inter/mod.rs libs/BencodeRust/src/variables/inter/bencode_type.rs libs/BencodeRust/src/variables/inter/bencode_variable.rs libs/BencodeRust/src/variables/bencode_number.rs libs/BencodeRust/src/variables/bencode_object.rs libs/BencodeRust/src/variables/bencode_array.rs libs/BencodeRust/src/variables/bencode_bytes.rs libs/BencodeRust/src/utils/mod.rs libs/BencodeRust/src/utils/ordered_map.rs
+
+/home/brad/Projects/bittorrent-dht-bootstrap/rust/target/debug/deps/bencode-d8bf5768ef905017.d: libs/BencodeRust/src/lib.rs libs/BencodeRust/src/variables/mod.rs libs/BencodeRust/src/variables/inter/mod.rs libs/BencodeRust/src/variables/inter/bencode_type.rs libs/BencodeRust/src/variables/inter/bencode_variable.rs libs/BencodeRust/src/variables/bencode_number.rs libs/BencodeRust/src/variables/bencode_object.rs libs/BencodeRust/src/variables/bencode_array.rs libs/BencodeRust/src/variables/bencode_bytes.rs libs/BencodeRust/src/utils/mod.rs libs/BencodeRust/src/utils/ordered_map.rs
+
+libs/BencodeRust/src/lib.rs:
+libs/BencodeRust/src/variables/mod.rs:
+libs/BencodeRust/src/variables/inter/mod.rs:
+libs/BencodeRust/src/variables/inter/bencode_type.rs:
+libs/BencodeRust/src/variables/inter/bencode_variable.rs:
+libs/BencodeRust/src/variables/bencode_number.rs:
+libs/BencodeRust/src/variables/bencode_object.rs:
+libs/BencodeRust/src/variables/bencode_array.rs:
+libs/BencodeRust/src/variables/bencode_bytes.rs:
+libs/BencodeRust/src/utils/mod.rs:
+libs/BencodeRust/src/utils/ordered_map.rs:
---
+++ rust/target/debug/deps/bittorrent_dht_bootstrap-48f5f67a901ca428.d
@@ -0,0 +1,6 @@
+/home/brad/Projects/bittorrent-dht-bootstrap/rust/target/debug/deps/bittorrent_dht_bootstrap-48f5f67a901ca428: src/main.rs src/daemon.rs
+
+/home/brad/Projects/bittorrent-dht-bootstrap/rust/target/debug/deps/bittorrent_dht_bootstrap-48f5f67a901ca428.d: src/main.rs src/daemon.rs
+
+src/main.rs:
+src/daemon.rs:
